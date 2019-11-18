7bdf3f0bbe3bfaf3b844868daf8f9b22348340f1
madprops <easywest@protonmail.com>
2019-11-17 19:10:24 -0600

    Help mod

--- src/config.nim
+++ src/config.nim
@@ -9,4 +9,5 @@ proc get_config*(): Config =
   let editor = use_arg(name="editor", kind="value", help="nano or vim", value="nano")
   let path = use_arg(name="path", kind="argument", help="Path to a script file")
   let run = use_arg(name="run", kind="flag", help="Run path automatically")
-  parse_args(); Config(editor:editor.val, path:path.val, run:run.used)
+  parse_args("dirfun - Create directories and files"); 
+  Config(editor:editor.val, path:path.val, run:run.used)
